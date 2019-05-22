fa9cf9df223112e705c5992203bf18396e22ce76
Marcus Pratley <mpratley@and.digital>
2019-05-22 14:34:39 +0100

    more linting changes!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,28 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-console": "off",
+    "no-debugger": "off",
+    "vue/array-bracket-spacing": "warning",
+    "vue/arrow-spacing": "warning",
+    "vue/brace-style": "warning",
+    "vue/camelcase": "error",
+    "vue/component-name-in-template-casing": "error",
+    "vue/eqeqeq": "warning",
+    "vue/match-component-file-name": "error",
+    "vue/object-curly-spacing": "warning",
+    "vue/script-indent": "warning",
+    "vue/space-unary-ops": "error"
   },
+
   parserOptions: {
     parser: "babel-eslint"
-  }
+  },
+
+  extends: ["plugin:vue/recommended", "@vue/prettier"]
 };
